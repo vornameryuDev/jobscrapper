dfa665de2d9c7dbbdfe039a25bc5b102e114064c
RyuMinSoo <sonicus94.dev@gmail.com>
2024-05-16 11:23:11 +0900

    modify index.html

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def search():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
